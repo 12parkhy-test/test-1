6c0d6976a1071af734ec359fd4117436584a1b16
12parkhy-test <12parkhy@kakao.com>
2020-09-12 19:58:28 +0900

    Updates

---
+++ precache-manifest.e36a932ccc4985fa07c759f369a3f636.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1ecb7ce485597efbd846c3c4b3acb7",
+    "url": "/test-1/index.html"
+  },
+  {
+    "revision": "b033e67b7b31bd70b663",
+    "url": "/test-1/static/css/main.2d887099.chunk.css"
+  },
+  {
+    "revision": "cc90428a343ae45e5c9e",
+    "url": "/test-1/static/js/2.accea60c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-1/static/js/2.accea60c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b033e67b7b31bd70b663",
+    "url": "/test-1/static/js/main.4c04d7a7.chunk.js"
+  },
+  {
+    "revision": "1662955e3658d22dc2ee",
+    "url": "/test-1/static/js/runtime-main.f0379b7b.js"
+  },
+  {
+    "revision": "92abaea0a98f58e1b40389d540ad3542",
+    "url": "/test-1/static/media/avatar-dark.92abaea0.svg"
+  },
+  {
+    "revision": "9a384b82ee739073864c3b700dfdb325",
+    "url": "/test-1/static/media/avatar.9a384b82.svg"
+  }
+]);
